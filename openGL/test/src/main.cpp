16933e2478e445b8e3107cd25a0081bf470170be
Nesterov-BA <nesterov.boris123@gmail.com>
2026-07-30 15:18:43 +0300

    openGl learning

---
+++ openGL/test/src/main.cpp
@@ -0,0 +1,82 @@
+#include <cmath>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader_s.h"
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+
+  // if (glfwGetKey(window, GLFW_KEY_1)) {
+  //   glClearColor(0.5f, 0.0f, 0.8f, 1.0f);
+  //   glClear(GL_COLOR_BUFFER_BIT);
+  // }
+  // if (glfwGetKey(window, GLFW_KEY_2)) {
+  //   glClearColor(0.0f, 0.0f, 0.8f, 1.0f);
+  //   glClear(GL_COLOR_BUFFER_BIT);
+  // }
+}
+
+int main() {
+  int success;
+  char infoLog[512];
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  std::cout << "a\n";
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  Shader ourShader("shaders/3.3.shader.vs", "shaders/3.3.shader.fs");
+
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  while (!glfwWindowShouldClose(window)) {
+    // input processing
+    processInput(window);
+    // render commands
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    ourShader.use();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // boilerplate
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  std::cout << "Terminated\n";
+  glfwTerminate();
+  return 0;
+}
